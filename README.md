848e9db90407be8671f6bda75772390e23170cd2
Рустам Кадиров <rustamkadirov4@gmail.com>
2025-08-12 10:59:56 +0500

    empty project skeleton

--- README.md
+++
@@ -1,3 +0,0 @@
-
-
-PR: review branch.
--- src/main/java/ru/rustam/catalog/catalog/repository/CatalogRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.rustam.catalog.catalog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.rustam.catalog.catalog.entity.CatalogEntity;
-
-public interface CatalogRepository extends JpaRepository<CatalogEntity, Integer> {
-}
