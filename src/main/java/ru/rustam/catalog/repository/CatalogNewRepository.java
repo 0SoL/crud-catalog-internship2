bf2028a3742da8411e808994d1057c36f60ed863
Рустам Кадиров <rustamkadirov4@gmail.com>
2025-08-25 13:27:43 +0500

    маппер для решения 1+n

--- src/main/java/ru/rustam/catalog/repository/CatalogNewRepository.java
+++ src/main/java/ru/rustam/catalog/repository/CatalogNewRepository.java
@@ -12,8 +12,8 @@ import ru.rustam.catalog.entity.CatalogEntity;
 import ru.rustam.catalog.entity.CategoryEntity;
 import ru.rustam.catalog.entity.FileEntity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.ResultSet;
+import java.util.*;
 
 @Repository
 public class CatalogNewRepository {
@@ -23,96 +23,134 @@ public class CatalogNewRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public Page<CatalogEntity> searchProduct(FilteredCatalogDto filteredCatalogDto, Pageable pageable) {
-        StringBuilder where = new StringBuilder(" WHERE 1=1 "); // читерное условие какое то спрсоить у Данияра
-        List<Object> params = new ArrayList<>();
+    private static final StringBuilder BASE_SELECT = new StringBuilder(
+            "SELECT c.* FROM catalog c "
+    );
 
+    private static final StringBuilder COUNT_SELECT = new StringBuilder(
+            "SELECT COUNT(*) FROM catalog c "
+    );
+
+    public Page<CatalogEntity> searchProduct(FilteredCatalogDto filteredCatalogDto, Pageable pageable) {
+        List<Object> params = new ArrayList<>();
+        List<String> filters = new ArrayList<>();
         if (StringUtils.hasText(filteredCatalogDto.getName())) {
-            where.append(" AND (LOWER(c.name) LIKE LOWER(?) OR LOWER(c.description) LIKE LOWER(?)) ");
+            filters.add("(LOWER(c.name) LIKE LOWER(?) OR LOWER(c.description) LIKE LOWER(?))");
             params.add("%" + filteredCatalogDto.getName() + "%");
             params.add("%" + filteredCatalogDto.getName() + "%");
         }
 
         if (filteredCatalogDto.getMinPrice() != null) {
-            where.append(" AND c.price >= ? ");
+            filters.add("c.price >= ?");
             params.add(filteredCatalogDto.getMinPrice());
         }
 
         if (filteredCatalogDto.getMaxPrice() != null) {
-            where.append(" AND c.price <= ? ");
+            filters.add("c.price <= ? ");
             params.add(filteredCatalogDto.getMaxPrice());
         }
 
         if (filteredCatalogDto.getHasImages() != null) {
             if (filteredCatalogDto.getHasImages()) {
-                where.append(" AND ? = TRUE AND EXISTS (SELECT 1 FROM image i WHERE i.catalog_id = c.id) ");
-                params.add(filteredCatalogDto.getHasImages());
+                filters.add("EXISTS (SELECT 1 FROM image i WHERE i.catalog_id = c.id)");
             } else {
-                where.append(" AND ? = FALSE AND NOT EXISTS (SELECT 1 FROM image i WHERE i.catalog_id = c.id) ");
-                params.add(filteredCatalogDto.getHasImages());
+                filters.add("NOT EXISTS (SELECT 1 FROM image i WHERE i.catalog_id = c.id)");
             }
         }
 
-        String countSql = "SELECT COUNT(*) FROM catalog c " + where;
-        Long total = jdbcTemplate.queryForObject(countSql, Long.class);
+        String where = filters.isEmpty() ? "" : "WHERE " + String.join(" AND ", filters);
+
+        StringBuilder countSql = new StringBuilder(COUNT_SELECT).append(where);
+
+        Long total = jdbcTemplate.queryForObject(countSql.toString(), params.toArray() ,Long.class);
         if (total == null) {
             return new PageImpl<>(List.of(), pageable, 0);
         }
 
-        StringBuilder selectSql = new StringBuilder(
-                "SELECT c.* " +
-                "FROM catalog c " +
-                where
-        );
+        StringBuilder selectSql = new StringBuilder(BASE_SELECT).append(where);
 
-        params.add(pageable.getPageSize());
-        params.add((int) pageable.getOffset());
+        System.out.println(selectSql);
+
         if (pageable.getSort().isSorted()) {
-            selectSql.append(" ORDER BY ");
-            Sort.Order order  = pageable.getSort().iterator().next();
-            String property = order.getProperty();
-            if (property.equalsIgnoreCase("name")) {
-                String sortBy = "c.name";
-                selectSql.append(sortBy).append(" ").append(order.getDirection());
-            } else if (property.equalsIgnoreCase("price")) {
-                String sortBy = "c.price";
-                selectSql.append(sortBy).append(" ").append(order.getDirection());
+            selectSql.append("ORDER BY ");
+            List<String> sort = new ArrayList<>();
+            for (Sort.Order item : pageable.getSort()) {
+                String property = item.getProperty();
+                sort.add(property + " " + item.getDirection());
             }
+            selectSql.append(String.join(", ", sort));
         }
 
         selectSql.append(" LIMIT ? OFFSET ?");
+        params.add(pageable.getPageSize());
+        params.add((int) pageable.getOffset());
+        // map в для поиска
 
         List<CatalogEntity> content = jdbcTemplate.query(
                 selectSql.toString(),
                 (rs, rowNum) -> {
                     CatalogEntity catalogEntity = new CatalogEntity();
-                    CategoryEntity categoryEntity = new CategoryEntity();
-                    FileEntity primaryImage = new FileEntity();
                     catalogEntity.setId(rs.getInt("id"));
                     catalogEntity.setName(rs.getString("name"));
                     catalogEntity.setDescription(rs.getString("description"));
                     catalogEntity.setPrice(rs.getBigDecimal("price"));
 
+                    CategoryEntity categoryEntity = new CategoryEntity();
                     categoryEntity.setId(rs.getInt("category_id"));
                     catalogEntity.setCategory(categoryEntity);
 
+                    FileEntity primaryImage = new FileEntity();
                     primaryImage.setId(rs.getInt("primary_image_id"));
                     catalogEntity.setPrimaryImage(primaryImage);
 
-                    List<FileEntity> images = jdbcTemplate.query(
-                            "SELECT id,name FROM image WHERE catalog_id = ?",
-                            (rsImg, rowNumImg) -> {
-                                FileEntity fileEntity = new FileEntity();
-                                fileEntity.setId(rsImg.getInt("id"));
-                                fileEntity.setName(rsImg.getString("name"));
-                                return fileEntity;
-                            },catalogEntity.getId()
-                    );
-                    catalogEntity.setImages(images);
+                    // Оптимизировать эту часть
+//                    List<FileEntity> images = jdbcTemplate.query(
+//                            "SELECT id,name FROM image WHERE catalog_id = ?",
+//                            (rsImg, rowNumImg) -> {
+//                                FileEntity fileEntity = new FileEntity();
+//                                fileEntity.setId(rsImg.getInt("id"));
+//                                fileEntity.setName(rsImg.getString("name"));
+//                                return fileEntity;
+//                            },catalogEntity.getId()
+//                    );
+//                    catalogEntity.setImages(images);
                     return catalogEntity;
 
-                },params.toArray()
+                },
+                params.toArray()
         );
+
+        if (content.isEmpty()) {
+            return new PageImpl<>(content, pageable, total);
+        }
+
+        List<Integer> catalogIds = content.stream()
+                .map(CatalogEntity::getId)
+                .toList();
+
+        String imagesSql = "SELECT id, name, catalog_id FROM image WHERE catalog_id IN (" +
+                String.join(",", Collections.nCopies(catalogIds.size(), "?")) + ")";
+
+        Map<Integer, List<FileEntity>> imagesMap = jdbcTemplate.query(
+                imagesSql,
+                (ResultSet rs) -> {
+                    Map<Integer, List<FileEntity>> map = new HashMap<>();
+                    while (rs.next()) {
+                        int catalogId = rs.getInt("catalog_id");
+                        FileEntity fileEntity = new FileEntity();
+                        fileEntity.setId(rs.getInt("id"));
+                        fileEntity.setName(rs.getString("name"));
+                        map.computeIfAbsent(catalogId, k -> new ArrayList<>()).add(fileEntity);
+                    }
+                    return map;
+                },
+                catalogIds.toArray()
+        );
+        if (imagesMap != null) {
+            content.forEach(catalog ->
+                    catalog.setImages(imagesMap.getOrDefault(catalog.getId(), new ArrayList<>()))
+            );
+        }
         return new PageImpl<>(content, pageable, total);
     }
 }
